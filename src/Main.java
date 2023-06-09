bd7bc785c1131b577d6572ddb7bf4fe272d4aa5b
lanel <nelson.lillian@student.greenriver.edu>
2023-06-09 12:07:39 -0700

    Added documentation to the project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+/**
+ * This is the entry point to my sample app.
+ *
+ * @author Lillian Nelson
+ * @verson 1.0
+ */
 public class Main
 {
+    /**
+     * Starts the application
+     *
+     * @param args comand-line args (not used)
+     */
+
     public static void main(String[] args)
     {
         System.out.println("Hello world!, from SDEV 220!");
